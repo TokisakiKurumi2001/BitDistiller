7b14fc50b5d44f06ef935f75a8117f7f5470e282
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2024-07-10 22:01:46 +0700

    Update repo

--- scripts/quantize.sh
+++ scripts/quantize.sh
@@ -1,3 +1,3 @@
-cd BitDistiller/quantization
+cd quantization
 
 CUDA_VISIBLE_DEVICES=0 python autoclip.py --model_path llama2_7b_hf --calib_dataset pile --quant_type int --w_bit 2 --q_group_size 128 --run_clip --dump_clip ./clip_cache/hf-llama2-7b/int2-g128.pt
