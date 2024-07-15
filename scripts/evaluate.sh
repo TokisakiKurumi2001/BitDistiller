a23a0dc05eabdc5155bd7cc5156e06c6c12d105a
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2024-07-15 11:07:10 +0700

    Evaluate

---
+++ scripts/evaluate.sh
@@ -0,0 +1,4 @@
+cd test/general
+python wiki_ppl.py --model ../../train/ckpts/hf-llama-2-7b/int2-g128/ --quant_type int --bits 2 --group_size 128
+# python llm_eval.py --model ../../train/ckpts/hf-llama-2-7b/int2-g128/ --eval_tasks hendrycksTest-* --test_set --bits 2 --group_size 128 --quant_type int --num_fewshot 5
+# python llm_eval.py --model ../../train/ckpts/hf-llama-2-7b/int2-g128/ --eval_tasks arc_challenge,winogrande,hellaswag,piqa --test_set --bits 2 --group_size 128 --quant_type int --num_fewshot 0 
